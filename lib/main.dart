be7cb373e50333cab10ab5a52c6816cbd0ba8b90
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-05-24 01:23:32 +0530

    Add Firebase configuration files and generated options for Android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:fitness_app/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
